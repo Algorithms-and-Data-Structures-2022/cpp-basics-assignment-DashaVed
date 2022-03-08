d48d4d73c9b43c42128c8b6e595ab40380f67de7
Dasha <dasha.ved93@gmail.com>
2022-03-09 01:30:42 +0300

    first attempt

--- src/code.cpp
+++ src/code.cpp
@@ -2,75 +2,135 @@
 
 #include <iostream>   // cout, printf
 #include <algorithm>  // fill, copy
+using namespace std;
 
 namespace assignment {
 
   // Task 1
   int squared_diff(int left, int right) {
 
-    // Write your code here ...
+    int diff = (left - right);
 
-    return 0;
+    return diff * diff;
   }
 
   // Task 2
   bool check_bit(int mask, int bit_pos) {
 
-    // Write your code here ...
+    if (bit_pos < 0 || mask < 0){
 
-    return false;
+        return false;
+
+        }else {
+
+          return mask & (1 << bit_pos);
+        }
   }
 
   // Task 3
   int max3(int left, int middle, int right) {
 
-    // Write your code here ...
-
-    return 0;
+    if (left > middle){
+      if (left > right){
+        return left;
+      }else{
+        return right;
+      }
+    } else{
+      if (middle > right){
+        return middle;
+      }else{
+        return right;
+      }
+    }
   }
 
   // Task 4
   void swap_args(int *left, int *right) {
 
-    // Write your code here ...
+    if (left == nullptr || right == nullptr){
+      *left = *left;
+      *right = *right;
+          }
+    else{
+      int element = *left;
+      *left = *right;
+      *right = element;
+    }
   }
 
   // Task 5
   int arr_sum(int *arr, int length) {
+    if (length <= 0 || arr == nullptr){
+      return 0;
+    }else{
+      int summa = 0;
 
-    // Write your code here ...
-
-    return 0;
+      for (int i = 0; i < length; i++){
+        summa += arr[i];
+      }
+      return summa;
+    }
   }
 
   // Task 6
   int *find_max_elem(int *arr, int length) {
+    if (length <= 0 || arr == nullptr){
+      return 0;
+    }else{
+      int* result = &arr[0];
+      int max_elem = arr[0];
 
-    // Write your code here ...
-
-    return nullptr;
+      for (int i = 1; i < length; i++){
+        if (arr[i] > max_elem){
+          max_elem = arr[i];
+          int* result = &arr[i];
+        }
+      }
+      return result;
+    }
   }
 
   // Task 7
   int *allocate_arr(int length, int init_value) {
 
-    // Write your code here ...
+    if (length == 0){
+        return nullptr;
+      }else{
 
-    return nullptr;
+        int arr[length];
+        for (int i = 0; i < length; i++){
+          arr[i] = init_value;
+        }
+        return arr;
+      }
   }
 
   // Task 8
   int *clone_arr(int *arr_in, int length) {
-
-    // Write your code here ...
-
-    return nullptr;
+    if (length <= 0 || arr_in == nullptr){
+      return 0;
+    }else{
+      int arr_copy[length];
+      std::copy(arr_in, arr_in + length, arr_copy);
+      return 0;
+    }
   }
 
   // Task 9
   void print_kth_elements(int *arr, int length, int k, std::ostream &os) {
 
-    // Write your code here ...
+    if (arr == nullptr){
+      cout << 'Invalid argument: arr\n' << endl;
+    }else if(length <= 0){
+      cout << "Invalid argument: length\n" << endl;
+    }else if (k <= 0){
+      cout << "Invalid argument: k\n" << endl;
+    }else{
+      for (int i = 0; i < length; i += k){
+        cout << arr[i] << endl;
+      }
+    }
   }
 
 }  // namespace assignment
